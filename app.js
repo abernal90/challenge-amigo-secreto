4e3e4c57add3a58e0c6308f6ccda0de74e991c71
IEBEAA01 <abernal@externo.infonavit.org.mx>
2025-01-28 10:48:59 -0600

    Se implementa función para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+function agregarAmigo(){
+   let amigo = document.getElementById('amigo').value;
+   if(amigo === ''){
+        alert('Por favor, inserte un nombre.');
+   } else{
+        amigos.push(amigo);
+        limpiarCaja();
+   }
+   console.log(amigos);
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
